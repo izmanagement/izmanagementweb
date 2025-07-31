b4992eea667c8df85c71fc4469b4b609836806a9
smmiz <smmizmodels@gmail.com>
2025-07-30 22:22:15 -0600

    Integración de Meta Pixel y ajustes finales

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        {/* INICIO DEL CÓDIGO DE GOOGLE ANALYTICS */}
+        {/* CÓDIGO DE GOOGLE ANALYTICS */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-T9FHD55TVL"
@@ -27,7 +27,28 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-T9FHD55TVL');
           `}
         </Script>
-        {/* FIN DEL CÓDIGO DE GOOGLE ANALYTICS */}
+
+        {/* INICIO DEL CÓDIGO DE META PIXEL */}
+        <Script id="meta-pixel-script">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '917901933863300');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+          src="https://www.facebook.com/tr?id=917901933863300&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* FIN DEL CÓDIGO DE META PIXEL */}
       </head>
       <body className={interTight.className}>{children}</body>
     </html>
